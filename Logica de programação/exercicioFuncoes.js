928b9e5cfd8ff7ff16e45b78c149d95706f89266
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-11-07 19:26:44 -0300

    teste de funcões

---
+++ Logica de programação/exercicioFuncoes.js
@@ -0,0 +1,19 @@
+
+function escreverMeuNome(nome){
+    console.log('Meu nome é ' + nome + '.');
+}
+
+escreverMeuNome('Eric');
+
+
+function testarIdade(idade) {
+    if (idade>= 18) {
+        console.log('E eu sou maior de idade');
+    } else {
+        console.log('E eu sou Menor de idade')
+    }
+}
+
+testarIdade(15);
+
+
